2bc3f0174a70c1ccce6fcb9aca99336c89dc4a68
Mike M <mike.d.meier@gmail.com>
2011-10-02 12:00:39 -0600

    break out FoodNetwork.ca from GreatTV.ca

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -3,37 +3,14 @@ import re, string, datetime, operator
 
 ####################################################################################################
 
-VIDEO_PREFIX = "/video/greattv.ca"
+VIDEO_PREFIX = "/video/food_network_canada"
 
 NAME = L('Title')
 
-ART             = 'art-default.png'
+ART             = 'art-default.jpg'
 ICON            = 'icon-default.png'
-DIY_ICON        = 'Diy-icon.png'
-DIY_ART         = 'Diy-art.png'
-FOOD_ICON       = 'Food-icon.png'
-FOOD_ART        = 'Food-art.png'
-GLOBAL_ICON     = 'Global-icon.png'
-GLOBAL_ART      = 'Global-art.png'
-HGTV_ICON       = 'HGTV-icon.png'
-HGTV_ART        = 'HGTV-art.png'
-HISTORY_ICON    = 'History-icon.png'
-HISTORY_ART     = 'History-art.png'
-SHOWCASE_ICON   = 'Showcase-icon.png'
-SHOWCASE_ART    = 'Showcase-art.png'
-SLICE_ICON      = 'Slice-icon.png'
-SLICE_ART       = 'Slice-art.png'
-TVTROPOLIS_ICON = 'TVTropolis-icon.png'
-TVTROPOLIS_ART  = 'TVTropolis-art.png'
 
-DIY_PARAMS          = ["FgLJftQA35gBSx3kKPM46ZVvhP6JxTYt", "z/DIY%20Network%20-%20Video%20Centre"]
 FOOD_PARAMS         = ["6yC6lGVHaVA8oWSm1F9PaIYc9tOTzDqY", "z/FOODNET%20Player%20-%20Video%20Centre"]
-GLOBALTV_PARAMS     = ["W_qa_mi18Zxv8T8yFwmc8FIOolo_tp_g", "z/Global%20Video%20Centre"]
-HGTV_PARAMS         = ["HmHUZlCuIXO_ymAAPiwCpTCNZ3iIF1EG", "z/HGTV%20Player%20-%20Video%20Center"]
-HISTORY_PARAMS      = ["IX_AH1EK64oFyEbbwbGHX2Y_2A_ca8pk", "z/History%20Player%20-%20Video%20Center"]
-SHOWCASE_PARAMS     = ["sx9rVurvXUY4nOXBoB2_AdD1BionOoPy", "z/Showcase%20Video%20Centre"]
-SLICE_PARAMS        = ["EJZUqE_dB8XeUUgiJBDE37WER48uEQCY", "z/Slice%20Player%20-%20New%20Video%20Center"]
-TVTROPOLIS_PARAMS   = ["3i9zvO0c6HSlP7Fz848a0DvzBM0jUWcC", "z/TVTropolis%20Player%20-%20Video%20Center"]
 
 FEED_LIST    = "http://feeds.theplatform.com/ps/JSON/PortalService/2.2/getCategoryList?PID=%s&startIndex=1&endIndex=500&query=hasReleases&query=CustomText|PlayerTag|%s&field=airdate&field=fullTitle&field=author&field=description&field=PID&field=thumbnailURL&field=title&contentCustomField=title&field=ID&field=parent"
 
@@ -55,21 +32,24 @@ def Start():
 
 
 ####################################################################################################
+
 def MainMenu():
     dir = MediaContainer(viewGroup="List")
+    
+    network = FOOD_PARAMS
+    
+    dir = MediaContainer(title2=sender.itemTitle, viewGroup="List", art=sender.art)
+    content = RSS.FeedFromURL("http://www.foodnetwork.ca/2086977.atom")
+    #Log(content)
 
-    dir.Append(Function(DirectoryItem(DiyPage, "diy Network", thumb=R(DIY_ICON), art=R(DIY_ART)), network = DIY_PARAMS))
-    dir.Append(Function(DirectoryItem(FoodPage, "Food Network", thumb=R(FOOD_ICON), art=R(FOOD_ART)), network = FOOD_PARAMS))
-    dir.Append(Function(DirectoryItem(GlobalPage, "Global TV", thumb=R(GLOBAL_ICON), art=R(GLOBAL_ART)), network = GLOBALTV_PARAMS))
-    dir.Append(Function(DirectoryItem(HGTVPage, "HGTV", thumb=R(HGTV_ICON), art=R(HGTV_ART)), network = HGTV_PARAMS))
-    dir.Append(Function(DirectoryItem(HistoryPage, "History", thumb=R(HISTORY_ICON), art=R(HISTORY_ART)), network = HISTORY_PARAMS))
-    dir.Append(Function(DirectoryItem(ShowcasePage, "Showcase", thumb=R(SHOWCASE_ICON), art=R(SHOWCASE_ART)), network = SHOWCASE_PARAMS))
-    dir.Append(Function(DirectoryItem(HistoryPage, "Slice", thumb=R(SLICE_ICON), art=R(SLICE_ART)), network = SLICE_PARAMS))
-    dir.Append(Function(DirectoryItem(TvtropolisPage, "TVTropolis", thumb=R(TVTROPOLIS_ICON), art=R(TVTROPOLIS_ART)), network = TVTROPOLIS_PARAMS))
-
+    for item in content['entries']:
+        title = item['title']
+        id = item['link'].split('=')[1]
+        dir.Append(Function(DirectoryItem(VideosPage, title), pid=network[0], id=id))
     return dir
     
 ####################################################################################################
+
 def VideoPlayer(sender, pid):
 
     videosmil = HTTP.Request(DIRECT_FEED % pid).content
@@ -100,6 +80,7 @@ def VideoPlayer(sender, pid):
     return Redirect(RTMPVideoItem(player, clip))
     
 ####################################################################################################
+
 def VideosPage(sender, pid, id):
 
     dir = MediaContainer(title2=sender.itemTitle, viewGroup="InfoList", art=sender.art)
@@ -121,185 +102,8 @@ def VideosPage(sender, pid, id):
     
     return dir
     
-#def ClipsPage(sender, showname):
-    #dir = MediaContainer(title2=sender.itemTitle, viewGroup="InfoList")
-    #dir.Append(Function(DirectoryItem(VideosPage, "Full Episodes"), clips="episode", showname=showname))
-    #dir.Append(Function(DirectoryItem(VideosPage, "Clips"), clips="", showname=showname))
-    #return dir
-####################################################################################################
-def FoodPage(sender, network):
-
-    dir = MediaContainer(title2=sender.itemTitle, viewGroup="List", art=sender.art)
-    content = RSS.FeedFromURL("http://www.foodnetwork.ca/2086977.atom")
-    #Log(content)
-
-    for item in content['entries']:
-        title = item['title']
-        id = item['link'].split('=')[1]
-        dir.Append(Function(DirectoryItem(VideosPage, title), pid=network[0], id=id))
-    return dir
-
-####################################################################################################
-def GlobalPage(sender, network):
-
-    dir = MediaContainer(title2=sender.itemTitle, viewGroup="List", art=sender.art)
-
-    content = JSON.ObjectFromURL(FEED_LIST % (network[0], network[1]))
-    for item in content['items']:
-        if item['title'] == "Full Episodes":
-            title = item['fullTitle']
-            title = title.split('/')[2]
-            id = item['ID']
-            dir.Append(Function(DirectoryItem(VideosPage, title), pid=network[0], id=id))
-    return dir
-    
 ####################################################################################################
 
-def HGTVPage(sender, network):
-    dir = MediaContainer(title2=sender.itemTitle, viewGroup="List", art=sender.art)
-
-    content = JSON.ObjectFromURL(FEED_LIST % (network[0], network[1]))
-    for item in content['items']:
-        if "Full Episodes" in item['parent']:
-            title = item['title']
-            id = item['ID']
-            thumb = item['thumbnailURL']
-            dir.Append(Function(DirectoryItem(VideosPage, title, thumb=thumb), pid=network[0], id=id))
-    return dir
-    
-####################################################################################################
-def HistoryPage(sender, network):
-
-    dir = MediaContainer(title2=sender.itemTitle, viewGroup="List", art=sender.art)
-    content = JSON.ObjectFromURL(FEED_LIST % (network[0], network[1]))
-    showList = {}
-    showCount = 0
-    #items = sorted(content['items'], key=attrgetter('fullTitle'))
-    items = content['items']
-    #Log(items)
-    items.sort(key=operator.itemgetter('fullTitle'))
-    #Log(items)
-    #parent = ""
-    for item in items:
-        if "Full Episodes" in item['fullTitle']:
-            title = item['fullTitle']
-            title = title.split('/')[1]
-            id = item['ID']
-            thumb = item['thumbnailURL']
-            #dir.Append(Function(DirectoryItem(VideosPage, title, thumb=thumb), pid=network[0], id=id))
-            try:
-                if showList[title]:
-                    discard = dir.Pop(showList[title]['index'])
-                    #Log('Removed: %s' %discard.title)
-                    dir.Append(Function(DirectoryItem(SeasonsPage, title, thumb=sender.thumb), network=network))
-                else:
-                    pass
-            except:
-                #Log('try failed')
-                #Log('showList does not contain %s' % title)
-                showList[title] = {'id':id, 'index':showCount}
-                showCount +=1
-                dir.Append(Function(DirectoryItem(VideosPage, title, thumb=sender.thumb), pid=network[0], id=id))
-    return dir
-            
-####################################################################################################
-def ShowcasePage(sender, network):
-    dir = MediaContainer(title2=sender.itemTitle, viewGroup="List", art=sender.art)
-    content = JSON.ObjectFromURL(FEED_LIST % (network[0], network[1]))
-    showList = {}
-    showCount = 0
-    #parent = ""
-    items = content['items']
-    items.sort(key=operator.itemgetter('fullTitle'))
-    for item in items:
-        if "SHOWVC/Shows/" in item['fullTitle']:
-            title = item['fullTitle']
-            #Log(title)
-            title = title.split('/')[2]
-            id = item['ID']
-            #thumb = item['thumbnailURL']
-            try:
-                if showList[title]:
-                    discard = dir.Pop(showList[title]['index'])
-                    #Log('Removed: %s' %discard.title)
-                    dir.Append(Function(DirectoryItem(SeasonsPage, title, thumb=sender.thumb), network=network))
-                else:
-                    pass
-            except:
-                #Log('try failed')
-                #Log('showList does not contain %s' % title)
-                showList[title] = {'id':id, 'index':showCount}
-                showCount +=1
-                dir.Append(Function(DirectoryItem(VideosPage, title, thumb=sender.thumb), pid=network[0], id=id))
-                
-    return dir
-            
-####################################################################################################
-def TvtropolisPage(sender, network):
-    dir = MediaContainer(title2=sender.itemTitle, viewGroup="List", art=sender.art)
-    content = JSON.ObjectFromURL(FEED_LIST % (network[0], network[1]))
-    showList = {}
-    showCount = 0
-    #parent = ""
-    items = content['items']
-    items.sort(key=operator.itemgetter('fullTitle'))
-    for item in items:
-        if "SPECTVVC/" in item['fullTitle']:
-            title = item['fullTitle']
-            #Log(title)
-            title = title.split('/')[1]
-            id = item['ID']
-            #thumb = item['thumbnailURL']
-            try:
-                if showList[title]:
-                    discard = dir.Pop(showList[title]['index'])
-                    #Log('Removed: %s' %discard.title)
-                    dir.Append(Function(DirectoryItem(SeasonsPage, title, thumb=sender.thumb), network=network))
-                else:
-                    pass
-            except:
-                #Log('try failed')
-                #Log('showList does not contain %s' % title)
-                showList[title] = {'id':id, 'index':showCount}
-                showCount +=1
-                dir.Append(Function(DirectoryItem(VideosPage, title, thumb=sender.thumb), pid=network[0], id=id))
-                
-    return dir
-            
-####################################################################################################
-def DiyPage(sender, network):
-    dir = MediaContainer(title2=sender.itemTitle, viewGroup="List", art=sender.art)
-    content = JSON.ObjectFromURL(FEED_LIST % (network[0], network[1]))
-    #Log(content)
-    showList = {}
-    showCount = 0
-    #parent = ""
-    items = content['items']
-    items.sort(key=operator.itemgetter('fullTitle'))
-    for item in items:
-        if "DIYVC/" in item['fullTitle']:
-            title = item['fullTitle']
-            #Log(title)
-            title = title.split('/')[1]
-            id = item['ID']
-            #thumb = item['thumbnailURL']
-            try:
-                if showList[title]:
-                    discard = dir.Pop(showList[title]['index'])
-                    #Log('Removed: %s' %discard.title)
-                    dir.Append(Function(DirectoryItem(SeasonsPage, title, thumb=sender.thumb), network=network))
-                else:
-                    pass
-            except:
-                #Log('try failed')
-                #Log('showList does not contain %s' % title)
-                showList[title] = {'id':id, 'index':showCount}
-                showCount +=1
-                dir.Append(Function(DirectoryItem(VideosPage, title, thumb=sender.thumb), pid=network[0], id=id))
-                
-    return dir
-            
-####################################################################################################
 def SeasonsPage(sender, network):
     dir = MediaContainer(title2=sender.itemTitle, viewGroup="List", art=sender.art)
     content = JSON.ObjectFromURL(FEED_LIST % (network[0], network[1]))
