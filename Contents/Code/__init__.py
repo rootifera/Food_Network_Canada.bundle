a1ffad6f7bca356cd990069b7c0c36987927136f
Mike <mike.d.meier@gmail.com>
2013-04-14 19:03:57 -0600

    framework 2.1 update

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,46 +1,40 @@
-import datetime, operator
-
-NAME = 'FoodNetwork.ca'
+####################################################################################################
+TITLE = "Food Network Canada"
 ART = 'art-default.jpg'
 ICON = 'icon-default.png'
-
-FOOD_PARAMS = ["6yC6lGVHaVA8oWSm1F9PaIYc9tOTzDqY", "z/FOODNET%20Player%20-%20Video%20Centre"]
-
+FOOD_PARAMS         = ["6yC6lGVHaVA8oWSm1F9PaIYc9tOTzDqY", "z/FOODNET%20Player%20-%20Video%20Centre"]
 FEED_LIST = "http://feeds.theplatform.com/ps/JSON/PortalService/2.2/getCategoryList?PID=%s&startIndex=1&endIndex=500&query=hasReleases&query=CustomText|PlayerTag|%s&field=airdate&field=fullTitle&field=author&field=description&field=PID&field=thumbnailURL&field=title&contentCustomField=title&field=ID&field=parent"
+FEEDS_LIST = "http://feeds.theplatform.com/ps/JSON/PortalService/2.2/getReleaseList?field=ID&field=contentID&field=PID&field=URL&field=airdate&PID=%s&contentCustomField=Episode&contentCustomField=Season&query=CategoryIDs|%s&field=thumbnailURL&field=title&field=length&field=description&startIndex=1&endIndex=500&sortField=airdate&sortDescending=true"
+DIRECT_FEED = "http://release.theplatform.com/content.select?format=SMIL&pid=%s&UserName=Unknown&Embedded=True"
 
-FEEDS_LIST = "http://feeds.theplatform.com/ps/JSON/PortalService/2.2/getReleaseList?PID=%s&startIndex=1&endIndex=500&query=categoryIDs|%s&query=BitrateEqualOrGreaterThan|400000&query=BitrateLessThan|601000&sortField=airdate&sortDescending=true&field=airdate&field=author&field=description&field=length&field=PID&field=thumbnailURL&field=title&contentCustomField=title"
+IGNORED = ["FOODNETVC", "Most Recent", "Video Blogs", "Video Bites", "FOODNETHOST"] # ' "Web Exclusives"
 
-DIRECT_FEED = "http://release.theplatform.com/content.select?format=SMIL&pid=%s&UserName=Unknown&Embedded=True&TrackBrowser=True&Tracking=True&TrackLocation=True"
-
-####################################################################################################
+###################################################################################################
 def Start():
-
-    Plugin.AddPrefixHandler("/video/foodnetworkcanada", MainMenu, NAME, ICON, ART)
-
-    Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
-    Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
-
-    MediaContainer.art = R(ART)
-    MediaContainer.title1 = NAME
-    DirectoryItem.thumb = R(ICON)
+    # Setup the default attributes for the ObjectContainer
+    ObjectContainer.title1 = TITLE
+    ObjectContainer.art = R(ART)
+    
+    # Setup the default attributes for the other objects
+    DirectoryObject.thumb = R(ICON)
+    DirectoryObject.art = R(ART)
+    EpisodeObject.thumb = R(ICON)
+    EpisodeObject.art = R(ART)
 
     HTTP.CacheTime = CACHE_1HOUR
 
 ####################################################################################################
+@handler("/video/food_network_canada", TITLE, ICON, ART)
 def MainMenu():
-
-    if not Platform.HasFlash:
-        return MessageContainer(NAME, L('This channel requires Flash. Please download and install Adobe Flash on the computer running Plex Media Server.'))
-
-    dir = MediaContainer(viewGroup="List")
-
+    oc = ObjectContainer()
     network = FOOD_PARAMS
 
     content = JSON.ObjectFromURL(FEED_LIST % (network[0], network[1]))
     showList = {}
-    showCount = 0
+    shows = []
+    shows_with_seasons = []
+
     items = content['items']
-    items.sort(key=operator.itemgetter('fullTitle'))
 
     for item in items:
         if item['parent'] == '':
@@ -48,109 +42,123 @@ def MainMenu():
         elif "/Web Exclusives/" in item['parent']:
             continue
         else: pass
+    
         title = item['parent'].split('/')[-1]
-
-        if title == "FOODNETVC":
+        if title in IGNORED:
             continue
         elif title == 'Food Network Classics':
             title = item['title']
         elif title == "TV Shows":
             title = item['title']
-        elif title == "Most Recent":
-            continue
-        elif title == "Video Blogs":
-            continue
-        elif title == "Video Bites":
-            continue
-        elif title == "Web Exclusives":
-            continue
         else:
             pass
-
-        id = item['ID']
+        
+        item_id = item['ID']
         thumb = item['thumbnailURL']
 
-        try:
-            if showList[title]:
-                discard = dir.Pop(showList[title]['index'])
-                dir.Append(Function(DirectoryItem(SeasonsPage, title), network=network))
+        if title in shows:
+            if title in shows_with_seasons:
+                continue
             else:
-                pass
-        except:
-            showList[title] = {'id':id, 'index':showCount}
-            showCount +=1
-            dir.Append(Function(DirectoryItem(VideosPage, title), pid=network[0], id=id))
-
-    dir.Sort('title')
-
-    return dir
-
-####################################################################################################
-
-def VideoPlayer(sender, pid):
-
-    videosmil = HTTP.Request(DIRECT_FEED % pid).content
-    player = videosmil.split("ref src")
-    player = player[2].split('"')
-
-    if ".mp4" in player[1]:
-        player = player[1].replace(".mp4", "")
-        try:
-            clip = player.split(";")
-            clip = "mp4:" + clip[4]
-        except:
-            clip = player.split("/video/")
-            player = player.split("/video/")[0]
-            clip = "mp4:/video/" + clip[-1]
-    else:
-        player = player[1].replace(".flv", "")
-        try:
-            clip = player.split(";")
-            clip = clip[4]
-        except:
-            clip = player.split("/video/")
-            player = player.split("/video/")[0]
-            clip = "/video/" + clip[-1]
-
-    return Redirect(RTMPVideoItem(player, clip))
+                oc.add(DirectoryObject(key=Callback(SeasonsPage, network=network, showtitle=title), title=title))
+                showList[title] = False
+                shows_with_seasons.append(title)
+        else:
+            shows.append(title)
+            showList[title] = {'title':title, 'id':item_id}
+    
+    for show in shows:
+        if showList[show]:
+            oc.add(DirectoryObject(key=Callback(VideosPage, pid=network[0], iid=showList[show]['id'], show=showList[show]['title']), title=showList[show]['title']))
+        else:
+            continue
+    
+    oc.objects.sort(key = lambda obj: obj.title)
+    return oc
 
 ####################################################################################################
-
-def VideosPage(sender, pid, id):
-
-    dir = MediaContainer(title2=sender.itemTitle, viewGroup="InfoList", art=sender.art)
-    pageUrl = FEEDS_LIST % (pid, id)
-    feeds = JSON.ObjectFromURL(pageUrl)
-
+def VideosPage(pid, iid, show):
+    oc = ObjectContainer()
+    pageURL = FEEDS_LIST % (pid, iid)
+    feeds = JSON.ObjectFromURL(pageURL)
+    
+    showList = {}
+    
     for item in feeds['items']:
         title = item['title']
-        pid = item['PID']
-        summary =  String.StripTags(item['description'].replace('In Full:', ''))
-        duration = item['length']
-        thumb = item['thumbnailURL']
-        airdate = int(item['airdate'])/1000
-        subtitle = 'Originally Aired: ' + datetime.datetime.fromtimestamp(airdate).strftime('%a %b %d, %Y')
-        dir.Append(Function(VideoItem(VideoPlayer, title=title, subtitle=subtitle, summary=summary, thumb=thumb, duration=duration), pid=pid))
+        try:
+            # show exists, skip adding multiples
+            if showList[title]:
+                continue
+        except:
+            # show doesn't exist, add it
+            showList[title]=""
+            pid = str(item['PID'])
+            iid = str(item['contentID'])
+            url = "http://www.foodnetwork.ca/video/video.html?v="+iid+"#video"
+            summary =  item['description']
+            duration = item['length']
+            thumb_url = item['thumbnailURL']
+            airdate = int(item['airdate'])/1000
+            originally_available_at = Datetime.FromTimestamp(airdate).date()
+                        
+            try:
+                # try to set the seasons and episode info
+                season = item['contentCustomData'][1]['value']
+                seasonint = int(float(season))
+                episode = item['contentCustomData'][0]['value']
+                episodeint = int(float(episode))
+                oc.add(
+                    EpisodeObject(
+                        url = url,
+                        title = title,
+                        summary=summary,
+                        duration=duration,
+                        thumb = Resource.ContentsOfURLWithFallback(url=thumb_url, fallback=ICON),
+                        originally_available_at = originally_available_at,
+                        season = seasonint,
+                        index = episodeint
+                        )
+                    )
 
-    dir.Sort('title')
-
-    return dir
+            except:
+                # if we don't get the season/episode info then don't set it
+                oc.add(
+                    EpisodeObject(
+                        url = url,
+                        title = title,
+                        summary=summary,
+                        duration=duration,
+                        thumb = Resource.ContentsOfURLWithFallback(url=thumb_url, fallback=ICON),
+                        originally_available_at = originally_available_at
+                        )
+                    )
+    
+    return oc
 
 ####################################################################################################
+def SeasonsPage(network, showtitle):
 
-def SeasonsPage(sender, network):
-
-    dir = MediaContainer(title2=sender.itemTitle, viewGroup="List", art=sender.art)
-    content = JSON.ObjectFromURL(FEED_LIST % (network[0], network[1]))
-
+    oc = ObjectContainer()
+    
+    pageURL = FEED_LIST % (network[0], network[1])
+    content = JSON.ObjectFromURL(pageURL)
+    season_list = []
+    
     for item in content['items']:
-        if sender.itemTitle in item['fullTitle']:
-            title = item['fullTitle']
-            title = title.split('/')[-1]
-            id = item['ID']
-
-            dir.Append(Function(DirectoryItem(VideosPage, title, thumb=sender.thumb), pid=network[0], id=id))
-
-    dir.Sort('title')
-
-    return dir
+        if showtitle in item['fullTitle']:
+            title = item['fullTitle'].split('/')[-1]
+            if title not in season_list and title != showtitle:
+                season_list.append(title)
+                iid = item['ID']
+                thumb_url = item['thumbnailURL']
+                oc.add(
+                    DirectoryObject(
+                        key = Callback(VideosPage, pid=network[0], iid=iid, show=showtitle),
+                            title = title,
+                            thumb = Resource.ContentsOfURLWithFallback(url=thumb_url, fallback=ICON)
+                            )
+                        )
+        
+    oc.objects.sort(key = lambda obj: obj.title)
+    return oc
