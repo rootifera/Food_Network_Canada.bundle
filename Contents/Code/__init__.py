91bc9d06d3125b7ab7da05843e076d16b6276ae1
zork22 <z22git@gmail.com>
2013-09-23 23:20:57 -0400

    Complete rewrite
    Rewrite the code entirely so it follows the structure of the website

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,164 +1,118 @@
-####################################################################################################
-TITLE = "Food Network Canada"
+VIDEO_URL = 'http://www.foodnetwork.ca/video.html?v='
+JSON_URL = 'http://feeds.theplatform.com/ps/JSON/PortalService/2.2/'
+FOODNETWORK_PARAMS = ['6yC6lGVHaVA8oWSm1F9PaIYc9tOTzDqY', 'z/FOODNVC%20-%20New%20Video%20Centre']
+JSON_CATEGORY_PARAMS = 'getCategoryList?field=ID&field=depth&field=hasReleases&field=fullTitle&PID={0}&query=CustomText|PlayerTag|{1}&field=title&field=fullTitle'
+JSON_RELEASE_PARAMS = 'getReleaseList?field=ID&field=contentID&field=PID&field=URL&field=categoryIDs&field=length&field=airdate&field=requestCount&PID={0}&contentCustomField=Show&contentCustomField=Episode&contentCustomField=Network&contentCustomField=Season&contentCustomField=Zone&contentCustomField=Subject&query=Categories|{1}&param=Site|shaw.foodnetwork.ca&query=CategoryIDs|{2}&field=thumbnailURL&field=title&field=length&field=description&field=assets&contentCustomField=Part&contentCustomField=Clip%20Type&contentCustomField=Web%20Exclusive&contentCustomField=ChapterStartTimes&contentCustomField=AlternateHeading&sortField=airdate&sortDescending=true'
+JSON_CATEGORY_FEED = JSON_URL + JSON_CATEGORY_PARAMS
 ART = 'art-default.jpg'
 ICON = 'icon-default.png'
-FOOD_PARAMS         = ["6yC6lGVHaVA8oWSm1F9PaIYc9tOTzDqY", "z/FOODNET%20Player%20-%20Video%20Centre"]
-FEED_LIST = "http://feeds.theplatform.com/ps/JSON/PortalService/2.2/getCategoryList?PID=%s&startIndex=1&endIndex=500&query=hasReleases&query=CustomText|PlayerTag|%s&field=airdate&field=fullTitle&field=author&field=description&field=PID&field=thumbnailURL&field=title&contentCustomField=title&field=ID&field=parent"
-FEEDS_LIST = "http://feeds.theplatform.com/ps/JSON/PortalService/2.2/getReleaseList?field=ID&field=contentID&field=PID&field=URL&field=airdate&PID=%s&contentCustomField=Episode&contentCustomField=Season&query=CategoryIDs|%s&field=thumbnailURL&field=title&field=length&field=description&startIndex=1&endIndex=500&sortField=airdate&sortDescending=true"
-DIRECT_FEED = "http://release.theplatform.com/content.select?format=SMIL&pid=%s&UserName=Unknown&Embedded=True"
+DEFAULT_VIDEO_ICON = ICON
 
-IGNORED = ["FOODNETVC", "Most Recent", "Video Blogs", "Video Bites", "FOODNETHOST"] # ' "Web Exclusives"
+from collections import defaultdict
 
-###################################################################################################
+
+####################################################################################################
+#Utility functions & variables
+
+def emptyList(): return []
+
+def cleanChildren():
+    '''
+    Weeds out from the children dict all the garbage data i.e the category with no sub category 
+    and no releases
+    '''
+    removeCandidates = []
+    for id, value in catInfo.iteritems():
+        hasReleases = value[1]
+        if len(children[id]) == 0 and not hasReleases:
+            removeCandidates.append(id)
+    for keys in children.keys():
+        children[keys] = [ x for x in children[keys] if x not in removeCandidates]
+        
+
+#I don't like having global variables but I can't pass them as parameters of a callback function
+catInfo = {}
+children = defaultdict(emptyList)
+####################################################################################################
+
+####################################################################################################
 def Start():
-    # Setup the default attributes for the ObjectContainer
-    ObjectContainer.title1 = TITLE
+    Plugin.AddPrefixHandler('/video/foodnetwork_ca', CategoryFinder, 'Food Network Canada', ICON, ART)
+    ObjectContainer.title1 = 'Food Network Canada'
     ObjectContainer.art = R(ART)
-    
-    # Setup the default attributes for the other objects
     DirectoryObject.thumb = R(ICON)
-    DirectoryObject.art = R(ART)
-    EpisodeObject.thumb = R(ICON)
-    EpisodeObject.art = R(ART)
 
-    HTTP.CacheTime = CACHE_1HOUR
 
 ####################################################################################################
-@handler("/video/food_network_canada", TITLE, ICON, ART)
-def MainMenu():
-    oc = ObjectContainer()
-    network = FOOD_PARAMS
-
-    content = JSON.ObjectFromURL(FEED_LIST % (network[0], network[1]))
-    showList = {}
-    shows = []
-    shows_with_seasons = []
-
-    items = content['items']
-
-    for item in items:
-        if item['parent'] == '':
-            continue
-        elif "/Web Exclusives/" in item['parent']:
-            continue
-        else: pass
-    
-        title = item['parent'].split('/')[-1]
-        if title in IGNORED:
-            continue
-        elif title == 'Food Network Classics':
-            title = item['title']
-        elif title == "TV Shows":
-            title = item['title']
-        else:
-            pass
-        
-        item_id = item['ID']
-        thumb = item['thumbnailURL']
-
-        if title in shows:
-            if title in shows_with_seasons:
-                continue
-            else:
-                oc.add(DirectoryObject(key=Callback(SeasonsPage, network=network, showtitle=title), title=title))
-                showList[title] = False
-                shows_with_seasons.append(title)
-        else:
-            shows.append(title)
-            showList[title] = {'title':title, 'id':item_id}
-    
-    for show in shows:
-        if showList[show]:
-            oc.add(DirectoryObject(key=Callback(VideosPage, pid=network[0], iid=showList[show]['id'], show=showList[show]['title']), title=showList[show]['title']))
-        else:
-            continue
-    
-    oc.objects.sort(key = lambda obj: obj.title)
-    return oc
 
 ####################################################################################################
-def VideosPage(pid, iid, show):
+@route('/video/foodnetwork_ca/CategoryFinder')
+def CategoryFinder():
+    '''
+    Parse the JSON file to find all the available categories
+    '''
+    network = FOODNETWORK_PARAMS
     oc = ObjectContainer()
-    pageURL = FEEDS_LIST % (pid, iid)
-    feeds = JSON.ObjectFromURL(pageURL)
+    jsonData = JSON.ObjectFromURL(JSON_CATEGORY_FEED.format(network[0], network[1]))
+    root = []
+    children.clear()
+    catInfo.clear()
+    fullTitleMap = defaultdict(emptyList)
     
-    showList = {}
-    
-    for item in feeds['items']:
+    for item in jsonData['items']:
+        fullTitle = item['fullTitle']        
         title = item['title']
-        try:
-            # show exists, skip adding multiples
-            if showList[title]:
-                continue
-        except:
-            # show doesn't exist, add it
-            showList[title]=""
-            pid = str(item['PID'])
-            iid = str(item['contentID'])
-            url = "http://www.foodnetwork.ca/video/video.html?v="+iid+"#video"
-            summary =  item['description']
-            duration = item['length']
-            thumb_url = item['thumbnailURL']
-            airdate = int(item['airdate'])/1000
-            originally_available_at = Datetime.FromTimestamp(airdate).date()
-                        
-            try:
-                # try to set the seasons and episode info
-                season = item['contentCustomData'][1]['value']
-                seasonint = int(float(season))
-                episode = item['contentCustomData'][0]['value']
-                episodeint = int(float(episode))
-                oc.add(
-                    EpisodeObject(
-                        url = url,
-                        title = title,
-                        summary=summary,
-                        duration=duration,
-                        thumb = Resource.ContentsOfURLWithFallback(url=thumb_url, fallback=ICON),
-                        originally_available_at = originally_available_at,
-                        season = seasonint,
-                        index = episodeint
-                        )
-                    )
-
-            except:
-                # if we don't get the season/episode info then don't set it
-                oc.add(
-                    EpisodeObject(
-                        url = url,
-                        title = title,
-                        summary=summary,
-                        duration=duration,
-                        thumb = Resource.ContentsOfURLWithFallback(url=thumb_url, fallback=ICON),
-                        originally_available_at = originally_available_at
-                        )
-                    )
+        id = item['ID']
+        hasReleases = item['hasReleases']
+        depth = item['depth']
+        
+        fullTitleMap[fullTitle] = id
+        catInfo[id] = title, hasReleases
+        if depth > 0:
+            parentIdx = fullTitle.rfind(title)
+            #We want the parent full title without the trailing /
+            parentId = fullTitleMap[fullTitle[:parentIdx-1]]
+            children[parentId].append(id)
+        else:
+            root.append(id)
+    cleanChildren()
+    #Remove FOODNVC from the list and add the children instead to mimic the category structure 
+    #of the website
+    root.remove(2304116546)
+    root = root + children[2304116546]
     
+    for catId in root:
+        cat, hasReleases = catInfo[catId]
+        if len(children[catId]) > 0 or hasReleases:
+            oc.add(DirectoryObject(
+                            key = Callback(addSubCategory, parentId=catId, 
+                                pid=network[0], site=network[1]),
+                            title = cat))
     return oc
 
-####################################################################################################
-def SeasonsPage(network, showtitle):
 
-    oc = ObjectContainer()
+@route('/video/foodnetwork_ca/addSubCategory')
+def addSubCategory(parentId, pid, site):
+    parentId = int(parentId)
+    parentTitle, hasReleases = catInfo[parentId]
+    oc = ObjectContainer(title2 = parentTitle)
     
-    pageURL = FEED_LIST % (network[0], network[1])
-    content = JSON.ObjectFromURL(pageURL)
-    season_list = []
-    
-    for item in content['items']:
-        if showtitle in item['fullTitle']:
-            title = item['fullTitle'].split('/')[-1]
-            if title not in season_list and title != showtitle:
-                season_list.append(title)
-                iid = item['ID']
-                thumb_url = item['thumbnailURL']
-                oc.add(
-                    DirectoryObject(
-                        key = Callback(VideosPage, pid=network[0], iid=iid, show=showtitle),
-                            title = title,
-                            thumb = Resource.ContentsOfURLWithFallback(url=thumb_url, fallback=ICON)
-                            )
-                        )
-        
-    oc.objects.sort(key = lambda obj: obj.title)
+    if hasReleases:
+        jsonData = JSON.ObjectFromURL(JSON_URL + JSON_RELEASE_PARAMS.format(pid, site, parentId))
+        for item in jsonData['items']:
+            thumb = item['thumbnailURL']
+            videoName = item['title']
+            description = item['description']
+            id = item['contentID']
+            video_url = VIDEO_URL + str(id)
+            oc.add(VideoClipObject(url=video_url, title=videoName, summary=description,
+                thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=DEFAULT_VIDEO_ICON)))
+                
+    for subCatId in children[parentId]:
+        subCatTitle, hasReleases = catInfo[subCatId]
+        if len(children[subCatId]) > 0 or hasReleases:
+            oc.add(DirectoryObject(
+                        key = Callback(addSubCategory, parentId=subCatId, pid=pid, site=site),
+                        title = subCatTitle))
+            
     return oc
